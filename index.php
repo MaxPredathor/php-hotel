4e96f4d1d24cf6e75236f576aa240e27924452e7
MaxPredathor <max.gilli98@gmail.com>
2023-11-29 14:49:41 +0100

    push completed

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php include __DIR__. '/partials/header.php'; 
-var_dump($hotels);
+// var_dump($hotels);
 ?>
 
 <main class="container">
     <div class="row">
-        <?php foreach ($hotels as $hotel) { ?>
-            <div class="col-12 col-md-4 col-lg-3">
-                
-            </div>
-        <?php } ?>
+        <table class="table">
+            <thead>
+                 <tr>
+                    <?php 
+                    $headerKeys = array_keys($hotels[0]);
+                    foreach ($headerKeys as $key) { ?>
+                        <th scope="col"><?php echo ucfirst(str_replace('_', ' ', $key)) ?></th>
+                    <?php } ?>    
+                </tr> 
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $hotel['name']; ?></th>
+                        <td><?php echo $hotel['description']; ?></td>
+                        <td><?php echo $hotel['parking']; ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?></td>
+                    </tr>
+                <?php } ?>  
+            </tbody>
+        </table>
     </div>
 </main>
 
