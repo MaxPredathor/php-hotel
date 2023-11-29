54fdf24f01147f9c59b091d02a0c96fe52390f1d
MaxPredathor <max.gilli98@gmail.com>
2023-11-29 15:56:25 +0100

    added second bonus vote filter

--- index.php
+++ index.php
@@ -11,23 +11,36 @@ function filterHotelsByParking($hotels, $filterOption = 'all') {
         } else {
             return true;
         }
-    });
-    
+    });        
 } 
-$selectedFilter = isset($_GET['parking_filter']) ? $_GET['parking_filter'] : 'all';
-$filteredHotels = filterHotelsByParking($hotels, $selectedFilter);
+
+function filterHotelsByVote($hotels, $minVote = 0) {
+    return array_filter($hotels, function ($hotel) use ($minVote) {
+        return $hotel['vote'] >= $minVote;
+    });
+}
+
+$selectedParkingFilter = isset($_GET['parking_filter']) ? $_GET['parking_filter'] : 'all';
+$selectedMinVote = isset($_GET['min_vote']) ? $_GET['min_vote'] : 0;
+
+$filteredByParking = filterHotelsByParking($hotels, $selectedParkingFilter);
+$filteredHotels = filterHotelsByVote($filteredByParking, $selectedMinVote);
 ?>
 <main class="container">
     <div class="row">
         <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <label for="filter_select">Filter by Parking:</label>
-            <select class="form-select w-50 d-inline-block" name="parking_filter" id="filter_select">
-                <option value="all" <?php if (!isset($_GET['parking_filter']) || $_GET['parking_filter'] === 'all') echo 'selected'; ?>>All Hotels</option>
+            <label class="px-2" for="filter_select">Filter by Parking:</label>
+            <select class="form-select w-25 d-inline-block" name="parking_filter" id="filter_select">
+                <option selected value="all" <?php if (!isset($_GET['parking_filter']) || $_GET['parking_filter'] === 'all') echo 'selected'; ?>>All Hotels</option>
                 <option value="available" <?php if (isset($_GET['parking_filter']) && $_GET['parking_filter'] === 'available') echo 'selected'; ?>>Hotels with Parking</option>
                 <option value="unavailable" <?php if (isset($_GET['parking_filter']) && $_GET['parking_filter'] === 'unavailable') echo 'selected'; ?>>Hotels without Parking</option>
             </select>
+            <label class="px-2" for="min_vote">Minimum Vote:</label>
+            <input class="form-control w-25 d-inline-block" type="number" name="min_vote" id="min_vote" value="<?php echo $selectedMinVote; ?>" min="0">
             <button class="btn btn-primary my-2" type="submit">Filter</button>
         </form>
+    </div>    
+    <div class="row">    
         <table class="table">
             <thead>
                 <tr>
@@ -50,6 +63,7 @@ $filteredHotels = filterHotelsByParking($hotels, $selectedFilter);
                 <?php } ?>  
             </tbody>
         </table>
+        i want js back :C
     </div>
 </main>
 
