68099c0efd1f55b974bfc9ee8a4f3815b82c7223
MaxPredathor <max.gilli98@gmail.com>
2023-11-29 13:57:09 +0100

    scaffolding

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include __DIR__. '/partials/header.php'; ?>
+
+<main>
+    
+</main>
+
+<?php include __DIR__. '/partials/footer.php'; ?>
---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+        <footer>
+            
+        </footer>
+
+        <!-- My JS -->
+        <script src="js/script.js"></script>
+    </body>
+</html>
